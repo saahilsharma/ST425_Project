67b78e5805ef77240324b2e24b9491b4145c6abb
Saahil Sarma <saahil.sarma@met.com>
2020-11-11 00:23:40 +0000

    Additional comments and simplified some logic

--- Part II_Q2.R
+++ Part II_Q2.R
@@ -12,8 +12,12 @@ correlations = seq(-0.95, 0.95, by=0.01)
 prob_2_part_a = rep(0,length(correlations))
 
 run_simulation(n,p,mu,var,correlations,FUN = function(index, XDF){
+  # Check if each r.v. is -1 < X < 4 
   t = XDF[XDF$V1 > -1 & XDF$V1 < 4 & XDF$V2 > -1 & XDF$V2 < 4 & XDF$V3 > -1 & XDF$V3 < 4 & XDF$V4 > -1 & XDF$V4 < 4 & XDF$V5 > -1 & XDF$V5 < 4 & XDF$V6 > -1 & XDF$V6 < 4 & XDF$V7 > -1 & XDF$V7 < 4 & XDF$V8 > -1 & XDF$V8 < 4,]
   t = na.omit(t)
+  
+  # Probability is just the number of rows where the above condition was true
+  # divided by the sample size (n)
   prob_2_part_a[index] <<- nrow(t)/n
 })
 plot(correlations, prob_part_a, type = "l")
@@ -23,7 +27,8 @@ plot(correlations, prob_part_a, type = "l")
 prob_2_part_b = rep(0,length(correlations))
 
 run_simulation(n,p,mu,var,correlations,FUN = function(index, XDF){
-  t = abs(XDF$V1) + abs(XDF$V2) + abs(XDF$V3) + abs(XDF$V4) + abs(XDF$V5) + abs(XDF$V6) + abs(XDF$V7) + abs(XDF$V8) 
+  # Check if the absolute sum of X1...X8 <- 8
+  t = rowSums(abs(XDF))
   prob_2_part_b[index] <<- length(t[t<=8])/n
 })
 
--- Part II_Q3.R
+++ Part II_Q3.R
@@ -11,14 +11,20 @@ correlations = seq(-0.99, 0.99, by=0.01)
 prob_3_part_a = rep(0,length(correlations))
 
 run_simulation(n,p,mu,var,correlations,FUN = function(index, XDF){
+  # First, lets condition on whether X2 > 0, which then intuitively
+  # (and numerically) changes the sample size
   X2GtThanZero = XDF[XDF$V2 > 0,]
-  new_n = nrow(X2GtThanZero)
+  new_n = nrow(X2GtThanZero) # New sample size
+  
+  # Base case: If new sample is zero, prob = 0
   if(new_n == 0) {
     prob[i] = 0
   }
   else {
-    X1GtThanZero = X2GtThanZero[X2GtThanZero$V1 > 0,]
-    result = X1GtThanZero[X1GtThanZero$V3 > 0,]
+    # We now count each row that has X1 AND X3 > 0
+    result = X2GtThanZero[X2GtThanZero$V1 > 0 & X2GtThanZero$V3 > 0,]
+    
+    # Prob is simply the count divided by the "new" (conditional) sample size
     prob_3_part_a[index] <<- nrow(result)/new_n  
   } 
 })
@@ -27,19 +33,25 @@ run_simulation(n,p,mu,var,correlations,FUN = function(index, XDF){
 prob_3_part_b = rep(0,length(correlations))
 
 run_simulation(n,p,mu,var,correlations,FUN = function(index, XDF){
+  # First, lets condition on whether X2 < 0,which then intuitively
+  # (and numerically) changes the sample size
   X2LtThanZero = XDF[XDF$V2 < 0,]
-  new_n = nrow(X2LtThanZero)
+  new_n = nrow(X2LtThanZero) # New sample size
+  
+  # Base case: If new sample is zero, prob = 0
   if(new_n == 0) {
     prob[i] = 0
   }
   else {
-    X1GtThanZero = X2LtThanZero[X2LtThanZero$V1 > 0,]
-    result = X1GtThanZero[X1GtThanZero$V3 > 0,]
+    # We now count each row that has X1 AND X3 > 0
+    result = X2LtThanZero[X2LtThanZero$V1 > 0 & X2LtThanZero$V3 > 0,]
+    
+    # Prob is simply the count divided by the "new" (conditional) sample size
     prob_3_part_b[index] <<- nrow(result)/new_n  
   } 
 })
 
 # Plotting the two together
 par(mfrow = c(2,1))
-plot(x = correlations, y = prob_3_part_a, type = 'l', xlab = 'Correlation', ylab = 'Probability', main = 'P(X1 > 0, X3 > 0|X2 > 0)')
-plot(x = correlations, y = prob_3_part_b, type = 'l', xlab = 'Correlation', ylab = 'Probability', main = 'P(X1 > 0, X3 > 0|X2 < 0)')
+plot(x = correlations, y = prob_3_part_a, type = 'l', xlab = 'Correlation', ylab = 'Probability', main = 'P(X1 > 0, X3 > 0 | X2 > 0)')
+plot(x = correlations, y = prob_3_part_b, type = 'l', xlab = 'Correlation', ylab = 'Probability', main = 'P(X1 > 0, X3 > 0 | X2 < 0)')
