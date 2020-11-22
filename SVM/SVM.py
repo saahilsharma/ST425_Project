6d42ceac3740bcdede4d5638184742774bf42084
Saahil Sarma <saahil.sarma@met.com>
2020-11-22 10:16:53 +0000

    Cleanup

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -18,7 +18,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_score, recall_score, f1_score #for model evaluation
 from sklearn.metrics import confusion_matrix #for model evaluation
 from sklearn.model_selection import cross_val_score
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_predict
 
@@ -26,13 +26,16 @@ def plot_confusion_matrix(cm, title):
     ax= plt.subplot()
     sns.heatmap(cm, annot=True, ax = ax)
     ax.set_xlabel('Predicted labels'),ax.set_ylabel('True labels')
-    ax.set_title('Confusion Matrix ' + title);
-    ax.xaxis.set_ticklabels(['Did not die', 'Died']), ax.yaxis.set_ticklabels(['Did not die', 'Died'])
+    ax.set_title('Confusion Matrix ' + title)
+    ax.xaxis.set_ticklabels(['Did not die', 'Died'])
+    ax.yaxis.set_ticklabels(['Did not die', 'Died'])
     plt.show()
 
 
 def fit_ml_algo(algo, name, X_train, Y_train, X_test, Y_test, cv, storage):
-    """ Focus, on precision score, recall score and F1 score (balance between precision and recall)"""
+    # Focus, on precision score, recall score
+    # and F1 score (balance between precision and recall)
+
     storage['Name'].append(name)
     # TRAIN
     train_pred = cross_val_predict(algo, X_train, Y_train, cv=cv)
@@ -96,16 +99,27 @@ X = df_scaled.drop(['DEATH_EVENT'], axis = 1)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, random_state=42)
 
 #  Dataframe to store results of each method
-storage = {'Train F1' : [], 'Train precision': [], 'Train recall' : [], 'Test F1': [], 'Test precision' :[],
+storage = {'Train F1' : [], 'Train precision': [], 'Train recall' : [],
+           'Test F1': [], 'Test precision' :[],
           'Test recall' : [], 'Name' : []}
 
 
-fit_ml_algo(LogisticRegression(class_weight = 'balanced'), 'LR', X_train, Y_train, X_test, Y_test, 10, storage)
-fit_ml_algo(LinearDiscriminantAnalysis(), 'LDA', X_train, Y_train, X_test, Y_test, 10, storage)
-fit_ml_algo(SVC(kernel='linear', class_weight = 'balanced'), 'SVM Linear with C = 1 (Default)', X_train, Y_train, X_test, Y_test, 10, storage)
+fit_ml_algo(LogisticRegression(class_weight = 'balanced'), 'LR',
+            X_train, Y_train,
+            X_test, Y_test, 10, storage)
+
+fit_ml_algo(LinearDiscriminantAnalysis(), 'LDA',
+            X_train, Y_train,
+            X_test, Y_test, 10, storage)
+
+fit_ml_algo(SVC(kernel='linear', class_weight = 'balanced'),
+            'SVM Linear with C = 1 (Default)', X_train, Y_train,
+            X_test, Y_test, 10, storage)
 
 C_f1 = find_optimized_tuining_param(list(range(1, 40)))
-fit_ml_algo(SVC(kernel='linear', C = C_f1, class_weight = 'balanced'), "SVM Linear with C = " + str(C_f1), X_train, Y_train, X_test, Y_test, 10, storage)
+fit_ml_algo(SVC(kernel='linear', C = C_f1, class_weight = 'balanced'),
+            "SVM Linear with C = " + str(C_f1), X_train, Y_train,
+            X_test, Y_test, 10, storage)
 
 dataframe = pd.DataFrame(storage, index = storage['Name'])
 dataframe.drop(['Name'], axis = 1, inplace = True)
